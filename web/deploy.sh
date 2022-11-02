12e82c85bd427b3b2c7f029d80e7154c996a80ff
Mads Nedergaard <nedergaardmads@gmail.com>
2022-11-02 21:22:16 +0100

    Updates package version and ensure we bump it on deployment

--- web/deploy.sh
+++ web/deploy.sh
@@ -4,7 +4,8 @@ BUCKET_NAME="gs://beta.electricitymaps.com"
 
 echo "Starting deployment..."
 
-#Create bucket (already done): gsutil mb -p tmrow-152415 -c regional -l europe-west1 $BUCKET_NAME
+#Create bucket (already done): 
+# gsutil mb -p tmrow-152415 -c regional -l europe-west1 $BUCKET_NAME
 
 # Upload files and set proper index page
 gsutil -h "Cache-Control:public,max-age=0" -m cp -a public-read -r dist/* $BUCKET_NAME
@@ -17,5 +18,11 @@ gsutil web set -m index.html -e 404.html $BUCKET_NAME
 
 # Set no-cache for certain files if required
 #gsutil setmeta -h "Cache-Control:no-cache,max-age=0" $BUCKET_NAME/*.json
+
+# Create new git tag and Github release
+VERSION=$(pnpm version prerelease --preid=beta)
+git tag -a $VERSION -m "$VERSION"
+git push origin $VERSION
+gh release create $VERSION --generate-notes --prerelease --repo electricitymaps/electricitymaps-contrib-rewrite
 
 echo "Done!"
