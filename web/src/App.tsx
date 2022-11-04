efbd2ad22a6866402dcf2d0419b1d25956279f9e
Mads Nedergaard <nedergaardmads@gmail.com>
2022-11-04 08:11:56 +0100

    Updates outer wrapper styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import ErrorBoundary from 'features/error-boundary/ErrorBoundary';
 import Header from 'features/header/Header';
 import type { ReactElement } from 'react';
@@ -6,18 +7,20 @@ import { lazy, Suspense } from 'react';
 const Map = lazy(async () => import('features/map/Map'));
 const LeftPanel = lazy(async () => import('features/panels/LeftPanel'));
 
+const mainStyles = clsx('flex h-screen w-screen flex-col');
+const innerStyles = clsx('relative flex flex-auto items-stretch');
 export default function App(): ReactElement {
   return (
     <Suspense fallback={<div />}>
-      <div className="h-full">
+      <main className={mainStyles}>
         <Header />
-        <div className="flex h-screen">
+        <div className={innerStyles}>
           <ErrorBoundary>
             <LeftPanel />
             <Map />
           </ErrorBoundary>
         </div>
-      </div>
+      </main>
     </Suspense>
   );
 }
