d7a3a93ea43b7cc34541f3f8a4b6b5667da92c71
Mads Nedergaard <nedergaardmads@gmail.com>
2022-11-22 00:06:59 +0100

    Makes translation-helper save during the process (Instead of waiting until every single item had been translated)

--- web/scripts/translation-helper.js
+++ web/scripts/translation-helper.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 const readline = require('readline-sync');
+const path = require('path');
+const process = require('process');
 require('colors');
 
-const LOCALES_PATH = './public/locales/';
+const LOCALES_PATH = path.join(__dirname, '..', 'public/locales/');
+
+function saveTemporaryStateToFile(key, language, file) {
+  fs.writeFileSync(`${LOCALES_PATH}/${language}.json`, JSON.stringify(file, null, 2));
+  console.info(`Saved ${key}...`.yellow.italic);
+}
 
 const languages = fs
   .readdirSync(LOCALES_PATH)
   .filter((f) => f !== 'en.json' && f.endsWith('.json'))
   .map((f) => f.replace('.json', ''));
 
+console.info('\nTranslation helper\n'.underline.bold.cyan);
+console.info(
+  ' WARNING:'.bgYellow.black,
+  'Input is only saved after completing a full group of translations!\n'.yellow
+);
 console.info(`Languages you can translate: ${languages.join(', ').green}`);
+
 const language = readline.question('Which language do you want to translate: ');
 if (!languages.includes(language)) {
   console.error(`${language} is not a translatable language!`.red);
@@ -27,7 +40,11 @@ function checkUntranslated(e, o, p) {
     }
   } else {
     Object.keys(e).forEach((key) => {
-      const t = checkUntranslated(e[key], o !== undefined ? o[key] : undefined, `${p + key}.`);
+      const t = checkUntranslated(
+        e[key],
+        o !== undefined ? o[key] : undefined,
+        `${p + key}.`
+      );
       if (t !== '') {
         if (o === undefined) {
           // eslint-disable-next-line no-param-reassign
@@ -40,5 +57,16 @@ function checkUntranslated(e, o, p) {
   return o;
 }
 
-fs.writeFileSync(`${LOCALES_PATH}/${language}.json`, JSON.stringify(checkUntranslated(en, other, ''), null, 2));
-console.info('Finished!');
+// Find top level keys in other
+Object.keys(en).forEach((key) => {
+  let isNew = other[key] ? false : true;
+  const t = checkUntranslated(en[key], other[key], `${key}.`);
+  if (t !== '') {
+    other[key] = t;
+    if (isNew) {
+      saveTemporaryStateToFile(key, language, other);
+    }
+  }
+});
+
+console.info('\nYou have translated everything!\n'.rainbow);
