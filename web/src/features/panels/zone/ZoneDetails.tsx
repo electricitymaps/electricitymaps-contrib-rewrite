d3534f61808bb205298669fec682ec6070386faa
Markus Killendahl <markus.killendahl@tmrow.com>
2022-11-24 19:45:03 +0100

    Set up display by emissions

--- web/src/features/panels/zone/ZoneDetails.tsx
+++ web/src/features/panels/zone/ZoneDetails.tsx
@@ -1,15 +1,18 @@
 import useGetZone from 'api/getZone';
 import BreakdownChart from 'features/charts/BreakdownChart';
 import CarbonChart from 'features/charts/CarbonChart';
+import EmissionChart from 'features/charts/EmissionChart';
 import PriceChart from 'features/charts/PriceChart';
 import { useAtom } from 'jotai';
 import { Navigate, useParams } from 'react-router-dom';
-import { timeAverageAtom } from 'utils/state';
+import { displayByEmissionsAtom, timeAverageAtom } from 'utils/state';
+import DisplayByEmissionToggle from './DisplayByEmissionToggle';
 import { ZoneHeader } from './ZoneHeader';
 
 export default function ZoneDetails(): JSX.Element {
   const { zoneId } = useParams();
   const [timeAverage] = useAtom(timeAverageAtom);
+  const [displayByEmissions] = useAtom(displayByEmissionsAtom);
   const { data } = useGetZone({
     enabled: Boolean(zoneId),
   });
@@ -28,15 +31,22 @@ export default function ZoneDetails(): JSX.Element {
   const datetimes = Object.keys(data.zoneStates).map((key) => new Date(key));
 
   return (
-    <div>
+    <div
+      className="mb-60" // Adding room to scroll past the time controller
+    >
       <ZoneHeader
         zoneId={zoneId}
         date="November 9, 2022 at 8:00"
         isEstimated
         isAggregated
       />
-      <CarbonChart datetimes={datetimes} timeAverage={timeAverage} />
-      <BreakdownChart timeAverage={timeAverage} datetimes={datetimes} />
+      <DisplayByEmissionToggle />
+      {displayByEmissions ? (
+        <EmissionChart datetimes={datetimes} timeAverage={timeAverage} />
+      ) : (
+        <CarbonChart datetimes={datetimes} timeAverage={timeAverage} />
+      )}
+      <BreakdownChart datetimes={datetimes} timeAverage={timeAverage} />
       <PriceChart datetimes={datetimes} timeAverage={timeAverage} />
     </div>
   );
