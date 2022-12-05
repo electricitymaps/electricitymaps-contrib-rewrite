b59972b5c031292296c3a076d51265539758cc8d
Mads Nedergaard <nedergaardmads@gmail.com>
2022-12-05 22:12:34 +0100

    Adds "by source" label

--- web/src/features/charts/bar-breakdown/BarBreakdownChart.tsx
+++ web/src/features/charts/bar-breakdown/BarBreakdownChart.tsx
@@ -1,16 +1,19 @@
 import { useAtom } from 'jotai';
 import { PulseLoader } from 'react-spinners';
+import { useTranslation } from 'translation/translation';
+import { TimeAverages } from 'utils/constants';
 import { displayByEmissionsAtom } from 'utils/state';
 import { useRefWidthHeightObserver } from 'utils/viewport';
 import useBarBreakdownChartData from '../hooks/useBarBreakdownProductionChartData';
 import BarBreakdownEmissionsChart from './BarBreakdownEmissionsChart';
 import BarBreakdownProductionChart from './BarBreakdownProductionChart';
 
-function BarBreakdownChart() {
+function BarBreakdownChart({ timeAverage }: { timeAverage: TimeAverages }) {
   const { data, productionData, exchangeData, isLoading, height } =
     useBarBreakdownChartData();
   const [displayByEmissions] = useAtom(displayByEmissionsAtom);
   const { ref, width } = useRefWidthHeightObserver();
+  const { __ } = useTranslation();
 
   if (isLoading) {
     // TODO: Replace with skeleton graph (maybe full graph with no data?)
@@ -29,7 +32,15 @@ function BarBreakdownChart() {
   };
 
   return (
-    <div className="relative w-full" ref={ref}>
+    <div className="relative w-full text-md" ref={ref}>
+      <div className="relative top-2 mb-1 text-sm">
+        {__(
+          timeAverage !== TimeAverages.HOURLY
+            ? 'country-panel.averagebysource'
+            : 'country-panel.bysource'
+        )}
+      </div>
+
       {displayByEmissions ? (
         <BarBreakdownEmissionsChart
           data={data}
