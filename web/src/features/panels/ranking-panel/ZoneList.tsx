f5d0d6724801d459c9c8dd06d2a6629107ae188b
tonypls <tonyvanswet@gmail.com>
2022-12-07 11:19:10 +0100

    update scroll bar style and finish ranknig panel

--- web/src/features/panels/ranking-panel/ZoneList.tsx
+++ web/src/features/panels/ranking-panel/ZoneList.tsx
@@ -3,7 +3,6 @@ import InternalLink from 'components/InternalLink';
 import type { ReactElement } from 'react';
 import { HiChevronRight } from 'react-icons/hi2';
 import { GridState } from 'types';
-import { CountryTag } from '../zone/CountryTag';
 
 interface ZonelistProperties {
   data: ZoneRowType[];
@@ -21,25 +20,25 @@ export interface ZoneRowType {
 function ZoneRow({ zoneId, color, ranking, countryName, zoneName }: ZoneRowType) {
   return (
     <InternalLink
-      className="group my-1 flex h-11 w-full items-center overflow-hidden rounded bg-gray-100 pl-3  text-left hover:bg-gray-200 dark:bg-gray-700"
+      className="group my-[0.3rem] flex h-11 w-full items-center overflow-hidden rounded bg-gray-100 pl-3  text-left hover:bg-gray-200 dark:bg-gray-700"
       key={ranking}
       to={`/zone/${zoneId}`}
       data-test-id="zone-list-link"
     >
-      <p className=" flex w-4 justify-end pr-2 text-sm">{ranking}</p>
+      <p className=" flex w-4 justify-end pr-2 text-xs">{ranking}</p>
       <div
-        className="mr-2 h-4 w-4 min-w-[16px] rounded"
+        className="mr-2 h-4 w-4 min-w-[16px] rounded-sm	"
         style={{ backgroundColor: color }}
       ></div>
 
-      <CountryTag zoneId={zoneId} showFlag />
+      <CountryFlag size={30} zoneId={zoneId} />
       <div className="flex flex-grow items-center justify-between overflow-hidden">
-        <div className="flex  flex-col justify-center overflow-hidden px-2 ">
-          <p className="truncate font-poppins text-sm leading-none">{countryName}</p>
+        <div className="flex  flex-col justify-center overflow-hidden px-2 content-center pt-1">
+          <p className="truncate font-normal text-[12px]  leading-none ">{countryName}</p>
           <p
             className={`${
               countryName
-                ? 'truncate font-poppins text-xs text-gray-400'
+                ? 'truncate font-poppins text-xs text-gray-500'
                 : 'truncate font-poppins text-sm '
             }`}
           >
--- web/src/features/panels/zone/CountryTag.tsx
+++ web/src/features/panels/zone/CountryTag.tsx
@@ -3,17 +3,15 @@ import { getCountryName } from 'translation/translation';
 
 export function CountryTag({
   zoneId,
-  showFlag,
 }: {
   zoneId: string;
-  showFlag?: boolean;
   size?: number;
 }) {
   const countryName = getCountryName(zoneId);
 
   return (
     <span className="inline-flex h-5 items-center gap-1 rounded-full bg-gray-200 px-2 py-0.5 text-xs dark:bg-gray-900">
-      {showFlag && <CountryFlag zoneId={zoneId} />}
+      <CountryFlag zoneId={zoneId} />
       <span>{countryName}</span>
     </span>
   );
