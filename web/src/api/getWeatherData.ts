00f8906bedbef09756a279856a904c2d0c9c3976
Mads Nedergaard <nedergaardmads@gmail.com>
2022-12-16 21:23:59 +0100

    Updates data-fetching logic: - use correct dates - type data response - retry with prev. timestamp if it 404s

--- web/src/api/getWeatherData.ts
+++ web/src/api/getWeatherData.ts
@@ -4,59 +4,91 @@ import { useInterpolatedWindData } from 'features/weather-layers/hooks';
 
 import { REFETCH_INTERVAL_FIVE_MINUTES, getBasePath, getHeaders } from './helpers';
 
-function getBothForecastStartTime(now: Date) {
-  return sub(startOfHour(now), { hours: 9 }).toISOString();
+interface ForecastEntry {
+  data: number[];
+  header: {
+    dx: number;
+    dy: number;
+    forecastTime: number;
+    la1: number;
+    lo1: number;
+    nx: number;
+    ny: number;
+    parameterCategory: number;
+    parameterNumber: number;
+    refTime: string;
+  };
 }
+export type GfsForecastResponse = [ForecastEntry, ForecastEntry];
+
+const GFS_STEP_ORIGIN = 6; // hours
+const GFS_STEP_HORIZON = 1; // hours
+
+function getForecastStartTime(now: Date) {
+  // Warning: solar will not be available at horizon 0 so always do at least horizon 1
+  let origin = sub(startOfHour(now), { hours: GFS_STEP_HORIZON });
+  if (origin.getUTCHours() % GFS_STEP_ORIGIN !== 0) {
+    origin = getForecastStartTime(origin);
+  }
+  return origin;
+}
+
 function getBeforeForcastEndTime(now: Date) {
   return add(startOfHour(now), { hours: 0 }).toISOString();
 }
 
 function getAfterForecastEndTime(now: Date) {
-  return add(startOfHour(now), { hours: 1 }).toISOString();
+  return add(startOfHour(now), { hours: GFS_STEP_HORIZON }).toISOString();
 }
 
-export async function fetchGfsForecastBefore(resource: string, targetTime: Date) {
-  const path = `/v3/gfs/${resource}?refTime=${getBothForecastStartTime(
-    targetTime
-  )}&targetTime=${getBeforeForcastEndTime(targetTime)}`;
+const targetTimeFunction = {
+  before: getBeforeForcastEndTime,
+  after: getAfterForecastEndTime,
+};
+
+export async function fetchGfsForecast(
+  resource: string,
+  startTime: Date,
+  endTime: Date,
+  type: 'before' | 'after',
+  retries = 0
+): Promise<GfsForecastResponse> {
+  const targetTime = targetTimeFunction[type](endTime);
+  const path = `/v3/gfs/${resource}?refTime=${startTime.toISOString()}&targetTime=${targetTime}`;
   const requestOptions: RequestInit = {
     method: 'GET',
     headers: await getHeaders(path),
   };
   const response = await fetch(`${getBasePath()}${path}`, requestOptions);
-
   if (response.ok) {
-    const { data } = (await response.json()) as { data: any };
-
+    const { data } = await response.json();
     return data;
   }
 
-  throw new Error(await response.text());
-}
-export async function fetchGfsForecastAfter(resource: string, targetTime: Date) {
-  const path = `/v3/gfs/${resource}?refTime=${getBothForecastStartTime(
-    targetTime
-  )}&targetTime=${getAfterForecastEndTime(targetTime)}`;
-  const requestOptions: RequestInit = {
-    method: 'GET',
-    headers: await getHeaders(path),
-  };
-  const response = await fetch(`${getBasePath()}${path}`, requestOptions);
-
-  if (response.ok) {
-    const { data } = (await response.json()) as { data: any };
-
-    return data;
+  if (retries >= 3 || response.status !== 404) {
+    // TODO: Handle this error gracefully and show message to users that wind layer is not available
+    // TODO: Log to Sentry if status is not 404
+    throw new Error(
+      `Failed to fetch GFS forecast after ${retries} retries (status: ${response.status})`
+    );
   }
 
-  throw new Error(await response.text());
+  // Retry again with a previous step date if request failed with 404
+  return fetchGfsForecast(
+    resource,
+    sub(startTime, { hours: GFS_STEP_ORIGIN }),
+    endTime,
+    type,
+    retries + 1
+  );
 }
 
 async function getWeatherData() {
   const now = new Date();
 
-  const before = fetchGfsForecastBefore('wind', now);
-  const after = fetchGfsForecastAfter('wind', now);
+  const startTime = getForecastStartTime(now);
+  const before = fetchGfsForecast('wind', startTime, now, 'before');
+  const after = fetchGfsForecast('wind', startTime, now, 'after');
 
   const forecasts = await Promise.all([before, after]).then((values) => {
     return values;
@@ -70,7 +102,7 @@ export const useGetWind = (options?: UseQueryOptions<any>): UseQueryResult<any> 
   return useQuery<any>(['wind'], async () => await getWeatherData(), {
     staleTime: REFETCH_INTERVAL_FIVE_MINUTES,
     refetchOnWindowFocus: false,
-
+    retry: false, // Disables retrying as getWeatherData handles retrying with new timestamps
     ...options,
   });
 };
