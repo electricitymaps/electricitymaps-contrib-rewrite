0356340652542c990681289a9a78b91cea75fb10
tonypls <tonyvanswet@gmail.com>
2022-12-20 14:32:03 +0100

    add sentry

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,14 +3,21 @@ import { useGetAppVersion } from 'api/getAppVersion';
 import LoadingOverlay from 'components/LoadingOverlay';
 import { OnboardingModal } from 'components/modals/OnboardingModal';
 import Toast from 'components/Toast';
-import ErrorBoundary from 'features/error-boundary/ErrorBoundary';
+import ErrorComponent from 'features/error-boundary/ErrorBoundary';
 import Header from 'features/header/Header';
 import MapControls from 'features/map-controls/MapControls';
 import { lazy, ReactElement, Suspense } from 'react';
 import LegendContainer from 'components/legend/LegendContainer';
 import TimeControllerWrapper from 'features/time/TimeControllerWrapper';
+import { ErrorBoundary, init } from '@sentry/react';
 
 const isProduction = import.meta.env.PROD;
+if (isProduction) {
+  init({
+    dsn: 'https://c6e9e4041eb74d22803b3f2bfb0c794d@o192958.ingest.sentry.io/1829181', //Electricitymap-app sentry project
+    tracesSampleRate: 1, //This will send 100% of errors to Sentry
+  });
+}
 
 const Map = lazy(async () => import('features/map/Map'));
 const LeftPanel = lazy(async () => import('features/panels/LeftPanel'));
@@ -26,12 +33,12 @@ export default function App(): ReactElement {
       : false;
 
   return (
-    <Suspense fallback={<div />}>
+    <Suspense fallback={<ErrorComponent />}>
       <main className="fixed flex h-screen w-screen flex-col">
         <ToastProvider duration={20_000}>
           <Header />
           <div className="relative flex flex-auto items-stretch">
-            <ErrorBoundary>
+            <ErrorBoundary fallback={<ErrorComponent />}>
               {isSuccess && isNewVersionAvailable && (
                 <Toast
                   title="A new app version is available"
