08ff79b2716df6325a687bd00b540d89d3d44a41
tonypls <tonyvanswet@gmail.com>
2022-12-28 12:07:12 +0100

    hide legends on small screens

--- web/src/components/legend/LegendContainer.tsx
+++ web/src/components/legend/LegendContainer.tsx
@@ -21,7 +21,7 @@ export default function LegendContainer(): ReactElement {
     windLayerToggle === ToggleOptions.ON && selectedDatetime.index === 24;
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex w-[224px]  flex-col rounded bg-white px-1 py-2 shadow dark:bg-gray-900">
+    <div className="invisible fixed bottom-4 right-4 z-50 flex w-[224px]  flex-col rounded bg-white px-1 py-2 shadow dark:bg-gray-900 sm:visible ">
       {isSolarLayerEnabled && <SolarLegend />}
       {isWindLayerEnabled && <WindLegend />}
       <Co2Legend />
