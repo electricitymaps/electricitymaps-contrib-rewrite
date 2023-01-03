1b0acaf74eb80ce65c406cd38dbbbcf465a705a8
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-03 15:44:33 +0100

    Avoids TS conversion as it was giving too many problems

--- web/scripts/generate-arrows.mjs
+++ web/scripts/generate-arrows.mjs
@@ -1,16 +1,24 @@
 // Requires `brew install imagemagick`
 
-const child_process = require('child_process');
-const fs = require('fs');
-const d3 = Object.assign({}, require('d3-array'), require('d3-collection'), require('d3-scale'));
+import child_process from 'child_process';
+import { range } from 'd3-array';
+import { scaleLinear } from 'd3-scale';
+import fs from 'fs';
 
-import { themes } from './src/helpers/themes';
-
+// Copied from '../src/hooks/colors.ts'
+const defaultCo2Scale = {
+  steps: [0, 150, 600, 750, 800],
+  colors: ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'],
+};
+const colorblindCo2Scale = {
+  steps: [0, 200, 400, 600, 800],
+  colors: ['#FFFFB0', '#E0B040', '#A06030', '#602020', '#000010'],
+};
 function generateArrows(prefix, scaleTheme) {
-  const co2color = d3.scaleLinear().domain(scaleTheme.steps).range(scaleTheme.colors);
+  const co2color = scaleLinear().domain(scaleTheme.steps).range(scaleTheme.colors);
 
   const colors = { nan: '#A9A9A9' };
-  const keys = d3.range(0, 800 + 80, 80);
+  const keys = range(0, 800 + 80, 80);
   keys.forEach((k) => {
     colors[k] = co2color(k);
   });
@@ -101,5 +109,5 @@ function generateArrows(prefix, scaleTheme) {
   // #convert -dispose none -delay 0 demo-arrow.png -dispose previous -delay 16x1000 -loop 0 highlight/*.png -layers coalesce animated.gif
 }
 
-generateArrows('', themes.dark.co2Scale);
-generateArrows('colorblind-', themes.colorblindDark.co2Scale);
+generateArrows('', defaultCo2Scale);
+generateArrows('colorblind-', colorblindCo2Scale);
