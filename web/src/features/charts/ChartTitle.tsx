ddbb321d42aefb6aa86c595ad62be9fcb2c1ad71
tonypls <tonyvanswet@gmail.com>
2023-01-06 15:03:59 +0100

    add get our data link

--- web/src/features/charts/ChartTitle.tsx
+++ web/src/features/charts/ChartTitle.tsx
@@ -2,6 +2,7 @@ import { useAtom } from 'jotai';
 import { useTranslation } from 'translation/translation';
 import { formatTimeRange } from 'utils/formatting';
 import { timeAverageAtom } from 'utils/state/atoms';
+import { HiOutlineArrowDownTray } from 'react-icons/hi2';
 
 type Props = {
   translationKey: string;
@@ -21,13 +22,26 @@ export function ChartTitle({ translationKey }: Props) {
   Use local for timeAverage if exists, otherwise use local default if exists. If no translation exists, use english
   */
   return (
-    <h3 className="text-md">
-      {localExists
-        ? __(`${translationKey}.${timeAverage}`)
-        : __(
-            `${translationKey}.default`,
-            formatTimeRange(localDefaultExists ? i18n.language : 'en', timeAverage)
-          )}
-    </h3>
+    <>
+      <h3 className="pt-3 pb-1 text-md">
+        {localExists
+          ? __(`${translationKey}.${timeAverage}`)
+          : __(
+              `${translationKey}.default`,
+              formatTimeRange(localDefaultExists ? i18n.language : 'en', timeAverage)
+            )}
+      </h3>
+      <div className="message flex flex-row items-center pb-2 text-xs no-underline">
+        <HiOutlineArrowDownTray size={12} />
+        <a
+          href="https://electricitymaps.com/?utm_source=app.electricitymap.org&utm_medium=referral&utm_campaign=country_panel"
+          target="_blank"
+          rel="noreferrer"
+          className="pl-0.5"
+        >
+          {__('country-history.Getdata')}
+        </a>
+      </div>
+    </>
   );
 }
