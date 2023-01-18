e3994af628dd40188a24657660bb5c7d3f61cb40
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-18 07:04:26 +0100

    Removes boilerplate code

--- web/src/testing/mocks/handlers.ts
+++ web/src/testing/mocks/handlers.ts
@@ -1,10 +1,8 @@
 import { rest } from 'msw';
-import fruits from './data/fruits.json';
 
+// TODO: We should either use this for mocking or remove it completely
 const handlers = [
-  rest.get('https://614c99f03c438c00179faa84.mockapi.io/fruits', (_, response, context) =>
-    response(context.json(fruits))
-  ),
+  rest.get('api_path', (_, response, context) => response(context.json(null))),
 ];
 
 export default handlers;
