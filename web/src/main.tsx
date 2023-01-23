2936e3ef12575b3faf77d4b2d298e0ecd71a067a
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-23 14:34:46 +0100

    Update web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -39,7 +39,7 @@ if (isProduction) {
         ),
       }),
     ],
-    tracesSampleRate: 0.1, //This will send 20% of transactions to Sentry
+    tracesSampleRate: 0.1, //This will send 10% of transactions to Sentry
   });
 }
 /**
