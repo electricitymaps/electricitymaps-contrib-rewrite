60c2e9ad5ca8cb151265327485146b32a634578f
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-23 16:13:22 +0100

    Replaces map with a back for old ios versions

--- web/src/features/map/MapWrapper.tsx
+++ web/src/features/map/MapWrapper.tsx
@@ -1,12 +1,15 @@
 import MapControls from 'features/map-controls/MapControls';
 import Map from './Map';
+import MapFallback, { isOldIOSVersion } from './MapFallback';
 import MapTooltip from './MapTooltip';
 
 export default function MapWrapper() {
+  const shouldShowFallback = isOldIOSVersion();
+
   return (
     <>
       <MapTooltip />
-      <Map />
+      {shouldShowFallback ? <MapFallback /> : <Map />}
       <MapControls />
     </>
   );
